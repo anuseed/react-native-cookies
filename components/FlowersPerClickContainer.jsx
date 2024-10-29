0a22a88ef19bb14a5f23fabd240912d032a46eb5
Anu Heese <anuheese@gmail.com>
2024-10-29 09:50:57 +0000

    flowerperclickcontainer to react native

---
+++ components/FlowersPerClickContainer.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function FlowersPerClickContainer() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Your flower power is 4 flowers per second.</Text>
+    </View>
+  );
+}
