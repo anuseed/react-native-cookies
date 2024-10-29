eb2536991148fad6e4c4fb5937853ab5de1a6c15
Anu Heese <anuheese@gmail.com>
2024-10-29 11:00:24 +0000

    added props, flowerCount and flowerPerSecondCount js functions

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
 import FlowerCountContainer from "./components/FlowerCountContainer";
 import FlowersPerClickContainer from "./components/FlowersPerClickContainer";
+// import FlowerUpgradesContainer from "./components/FlowerUgradesContainer";
 
 export default function App() {
+  const [flowersPerSecondCount, setFlowerPerSecondCount] = useState(1);
+  const [flowerCount, setFlowerCount] = useState(0);
+
+  function addFlower() {
+    setFlowerCount(flowerCount + 1);
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFlowerCount(
+        (currentFlowers) => currentFlowers + flowersPerSecondCount
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [flowersPerSecondCount]);
   return (
     <View style={styles.container}>
-      <FlowerCountContainer />
-      <FlowersPerClickContainer />
+      <FlowerCountContainer flowerCount={flowerCount} addFlower={addFlower} />
+      <FlowersPerClickContainer flowersPerSecondCount={flowersPerSecondCount} />
+      {/* <FlowerUpgradesContainer /> */}
 
       <StatusBar style="auto" />
     </View>
--- components/FlowerCountContainer.jsx
+++ components/FlowerCountContainer.jsx
@@ -1,10 +1,10 @@
 import { Image, View, Text } from "react-native";
 import flower from "../assets/flower.png";
 
-export default function FlowerCountContainer() {
+export default function FlowerCountContainer(props) {
   return (
     <View style={{ padding: 20 }}>
-      <Text>You have 4 flowers.</Text>
+      <Text>You have {props.flowerCount} flowers.</Text>
       <Image source={flower} style={{ width: 200, height: 200 }} />
     </View>
   );
